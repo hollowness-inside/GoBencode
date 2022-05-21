e6a27e7114fc04172883025eb4c147561b8a525f
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-05-21 18:40:12 +1000

    Examples organized

--- example/main.go
+++ example/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"fmt"
 	gobencode "joshua/green/bencode"
 	"os"
 )
 
 type Package struct {
-	Name  string   `bencode:"name"`
-	Date  int      `bencode:"date"`
-	Peers []string `bencode:"peers"`
+	Name  string
+	Date  int
+	Peers []string
 }
 
 func main() {
 	enc := gobencode.NewEncoder(os.Stdout)
-	// enc.Encode(52)
-	// enc.Encode("cats are cool")
-	// enc.Encode([]int{1, 2, 3, 4, 5, 6})
-	// enc.Encode([]string{"cats", "are", "cool"})
+	example_encode(enc)
+	example_struct_encode(enc)
+}
 
-	enc.Encode(Package{
+func example_encode(e *gobencode.Encoder) {
+	e.Encode(52)
+	e.Encode("cats are cool")
+	e.Encode([]int{1, 2, 3, 4, 5, 6})
+	e.Encode([]string{"cats", "are", "cool"})
+	fmt.Println()
+}
+
+func example_struct_encode(e *gobencode.Encoder) {
+	e.Encode(Package{
 		Name:  "gobencode",
 		Date:  1354534,
 		Peers: []string{"joshua", "codi", "mark"},
 	})
+	fmt.Println()
 }
